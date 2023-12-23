bfbb2ec516da4e1d4ea03bd749d9c570534875da
kerosin1323 <ermolenkokirill132@icloud.com>
2023-12-23 18:09:45 +0300

    Дополнение игры

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 import random
+
+import pygame.sprite
+
 from OneVSOne import *
 import SQL
 import registration
@@ -9,52 +12,66 @@ class Pitch:
     def __init__(self, screen, called, id1, id2=None):
         id2 = random.randint(0, 19) if id2 is None else id2
         # загрузка фона
-        background = pygame.image.load('image/map2.png')
-        screen.blit(background, (0, 0))
+
         fontObj = pygame.font.Font(None, 50)
         # счетчик таймера
+        self.goals1 = 0
+        self.goals2 = 0
+        background = pygame.image.load('image/map2.png')
+        screen.blit(background, (0, 0))
+
+        running = True
         countdown = 90
         TIMEREVENT = pygame.USEREVENT + 1
         pygame.time.set_timer(TIMEREVENT, 1000)
-        self.goals1 = 0
-        self.goals2 = 0
-        image_gate = pygame.image.load('image/gates1.png')
-        screen.blit(image_gate, (5, 285))
-        screen.blit(image_gate, (905, 285))
-        # создание флагов, которые выбрали игроки
         image = pygame.image.load(f'image/flags/{COUNTRIES_FLAG[COUNTRY[id1]]}.png')
         con_image = pygame.transform.scale(image, (90, 60))
         image2 = pygame.image.load(f'image/flags/{COUNTRIES_FLAG[COUNTRY[id2]]}.png')
         con_image2 = pygame.transform.scale(image2, (90, 60))
-        screen.blit(con_image2, (550, 510))
-        screen.blit(con_image, (350, 510))
-        running = True
+        player1 = Player(300, 340)
+        player2 = Player(700, 340)
+        left = right = False
         while running:
-            pygame.time.delay(100)
-            # создание счета
-            pygame.draw.rect(screen, (0, 0, 0), (460, 515, 70, 50))
-            self.score = fontObj.render(f'{self.goals1} : {self.goals2}', True, (255, 255, 255), None)
-            self.scoreRect = self.score.get_rect(center=(495, 540))
-            screen.blit(self.score, self.scoreRect)
+            timer = pygame.time.Clock()
+            timer.tick(60)
             for event in pygame.event.get():
+                keys = pygame.key.get_pressed()
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                elif event.type == TIMEREVENT:
-                    # изменение счетчика таймера
-                    pygame.draw.rect(screen, (0, 0, 0), (480, 10, 40, 40))
-                    self.textSurfaceObj = fontObj.render(f'{countdown}', True, (255, 255, 255), None)
-                    self.textRectObj = self.textSurfaceObj.get_rect(center=(500, 30))
-                    screen.blit(self.textSurfaceObj, self.textRectObj)
-                    countdown -= 1
-                    if countdown == -1:
-                        running = False
-                        EndGame(screen, id1, id2, self.goals1, self.goals2, called)
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_pos = pygame.mouse.get_pos()
                     if 5 < mouse_pos[0] < 95 and 250 < mouse_pos[1] < 494:
                         self.goals2 += 1
                     elif 905 < mouse_pos[0] < 995 and 250 < mouse_pos[1] < 494:
                         self.goals1 += 1
+                if event.type == TIMEREVENT:
+                    # изменение счетчика таймера
+                    countdown -= 1
+                    if countdown == -1:
+                        running = False
+                        EndGame(screen, id1, id2, self.goals1, self.goals2, called)
+                if keys[pygame.K_LEFT]:
+                    left = True
+                if keys[pygame.K_RIGHT]:
+                    right = True
+                if event.type == pygame.KEYUP:
+                    left = right = False
+                screen.blit(background, (0, 0))
+                Gates((10, 285), screen)
+                Gates((900, 285), screen)
+                # создание флагов, которые выбрали игроки
+                screen.blit(con_image2, (550, 510))
+                screen.blit(con_image, (350, 510))
+                # создание счета
+                self.score = fontObj.render(f'{self.goals1} : {self.goals2}', True, (0, 0, 0), None)
+                self.scoreRect = self.score.get_rect(center=(495, 540))
+                screen.blit(self.score, self.scoreRect)
+                self.textSurfaceObj = fontObj.render(f'{countdown}', True, (0, 0, 0), None)
+                self.textRectObj = self.textSurfaceObj.get_rect(center=(500, 30))
+                screen.blit(self.textSurfaceObj, self.textRectObj)
+                player1.update(left, right)
+                player1.draw(screen)
+                player2.draw(screen)
             pygame.display.flip()
 
 
@@ -103,3 +120,55 @@ class EndGame:
                     if 470 < mouse_pos[0] < 545 and 350 < mouse_pos[1] < 425:
                         pygame.time.delay(50)
                         firstwindow.Menu(self.screen)
+
+
+class Gates(pygame.sprite.Sprite):
+    def __init__(self, pos: tuple, screen, turn=False):
+        super().__init__()
+        self.screen = screen
+        self.pos = pos
+        self.image_gate = pygame.image.load('image/gates1.png')
+        self.imageSur = pygame.Surface((90, 205))
+        self.screen.blit(self.image_gate, pos)
+        self.rect = self.image_gate.get_rect()
+        self.rect.x, self.rect.y = pos
+
+    def check_goal(self):
+        horizontal_borders = pygame.sprite.Group()
+        return pygame.sprite.spritecollideany(Gates(self.pos, self.screen), horizontal_borders) \
+            and not pygame.sprite.collide_rect(Gates(self.pos, self.screen), Player(self.pos, self.screen))
+
+
+class Ball(pygame.sprite.Sprite):
+    pass
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.xvel = 0  # скорость перемещения. 0 - стоять на месте
+        self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
+        self.startY = y
+        self.image = pygame.Surface((80, 150))
+        self.image.fill((255, 0, 0))
+        self.rect = pygame.Rect(x, y, 80, 150)  # прямоугольный объект
+
+    def update(self, left, right):
+        if left:
+            self.xvel = -7  # Лево = x- n
+
+        if right:
+            self.xvel = 7  # Право = x + n
+
+        if not (left or right):  # стоим, когда нет указаний идти
+            self.xvel = 0
+
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+
+    def draw(self, screen):  # Выводим себя на экран
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+
+
